4d14f3a70138d5d9b62b472a1e20231e8651956e
David Tsai <david@matternet.us>
2020-02-21 15:28:38 -0800

    feat: add loading indicator

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import NextApp from 'next/app';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import Head from 'next/head';
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core';
 import theme from '../theme';
+
+Router.events.on('routeChangeStart', url => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 class App extends NextApp {
   render() {
     const { Component } = this.props;
 
     return (
-      <ThemeProvider theme={theme}>
-        <CSSReset />
-        <ColorModeProvider>
-          <Component />
-        </ColorModeProvider>
-      </ThemeProvider>
+      <>
+        <Head>
+          {/* Import CSS for nprogress */}
+          <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          <CSSReset />
+          <ColorModeProvider>
+            <Component />
+          </ColorModeProvider>
+        </ThemeProvider>
+      </>
     );
   }
 }
